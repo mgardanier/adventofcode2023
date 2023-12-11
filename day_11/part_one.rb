ef1f68a083208c14295695b25c5bc62d0441a77c
Michael Gardanier <mgardanier@simplenexus.com>
2023-12-11 15:58:05 -0700

    Part 1 done

--- day_11/part_one.rb
+++ day_11/part_one.rb
@@ -9,8 +9,40 @@ class PartOne
     parse_input
     get_expanded_rows
     get_expanded_columns
-    # use manhattan distance formula
-    print_map
+    distance = calculate_distance
+    # print_map
+    puts distance
+  end
+
+  def calculate_distance
+    galaxies = find_galaxies_positions
+    distances = {}
+    for i in 1..galaxies.length
+      for j in i + 1..galaxies.length
+        distances[[i,j]] = manhatten_distance(galaxies[i][1], galaxies[i][0], galaxies[j][1], galaxies[j][0])
+      end
+    end
+    return distances.values.sum
+  end
+
+  def manhatten_distance(x1, y1, x2, y2)
+    return (x1 - x2).abs + (y1 - y2).abs
+  end
+
+  def find_galaxies_positions
+    positions = {}
+    num = 1
+    for row in 0..@map.length - 1
+      row_offset = @expanded_rows.count { |element | element < row  }
+      for column in 0..@map[0].length - 1
+        column_offset = @expanded_columns.count { |element| element < column }
+        if @map[row][column] == '#'
+          positions[num] = [row + row_offset, column + column_offset]
+          num += 1
+        end
+      end
+    end
+    return positions
   end
 
   def get_expanded_columns
